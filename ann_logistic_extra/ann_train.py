5b99320f825d2a8babaf49e8429de9c2e6325608
thslehigh <thethansoe@gmail.com>
2016-08-04 13:18:56 -0600

    Update ann_train.py

--- ann_logistic_extra/ann_train.py
+++ ann_logistic_extra/ann_train.py
@@ -36,18 +36,15 @@ b2 = np.zeros(K)
 def softmax(a):
     expA = np.exp(a)
     return expA / expA.sum(axis=1, keepdims=True)
-
 def forward(X, W1, b1, W2, b2):
     Z = np.tanh(X.dot(W1) + b1)
     return softmax(Z.dot(W2) + b2), Z
-
 def predict(P_Y_given_X):
     return np.argmax(P_Y_given_X, axis=1)
 
 # calculate the accuracy
 def classification_rate(Y, P):
     return np.mean(Y == P)
-
 def cross_entropy(T, pY):
     return -np.mean(T*np.log(pY))
 

2f5d417374f2a859d52a8809d9fc025ac9edd3a1
thslehigh <thethansoe@gmail.com>
2016-08-15 21:54:08 -0600

    Update util.py

--- ann_class2/util.py
+++ ann_class2/util.py
@@ -1,32 +1,22 @@
-# Some utility functions we need for the class.
-# For the class Data Science: Practical Deep Learning Concepts in Theano and TensorFLow
-# https://www.udemy.com/data-science-deep-learning-in-theano-tensorflow
-
-# Note: run this from the current folder it is in.
-
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn.decomposition import PCA
 from sklearn.linear_model import LogisticRegression
 
-
 def get_transformed_data():
     print "Reading in and transforming data..."
     df = pd.read_csv('../large_files/train.csv')
     data = df.as_matrix().astype(np.float32)
     np.random.shuffle(data)
-
     X = data[:, 1:]
     mu = X.mean(axis=0)
     X = X - mu # center the data
     pca = PCA()
     Z = pca.fit_transform(X)
     Y = data[:, 0]
-
     return Z, Y, pca, mu
-
-
+    
 def get_normalized_data():
     print "Reading in and transforming data..."
     df = pd.read_csv('../large_files/train.csv')
@@ -40,7 +30,6 @@ def get_normalized_data():
     Y = data[:, 0]
     return X, Y
 
-
 def plot_cumulative_variance(pca):
     P = []
     for p in pca.explained_variance_ratio_:
@@ -51,7 +40,6 @@ def plot_cumulative_variance(pca):
     plt.plot(P)
     plt.show()
     return P
-
 
 def forward(X, W, b):
     # softmax
@@ -67,32 +55,22 @@ def forward(X, W, b):
     y = expa / expa.sum(axis=1, keepdims=True)
     # exit()
     return y
-
-
+    
 def predict(p_y):
     return np.argmax(p_y, axis=1)
-
-
 def error_rate(p_y, t):
     prediction = predict(p_y)
     return np.mean(prediction != t)
-
-
 def cost(p_y, t):
     # print "any nan in log p_y?:", np.any(np.isnan(np.log(p_y)))
     # print "log(p_y):", np.log(p_y)
     # exit()
     tot = t * np.log(p_y)
     return -tot.sum()
-
-
 def gradW(t, y, X):
     return X.T.dot(t - y)
-
-
 def gradb(t, y):
     return (t - y).sum(axis=0)
-
 
 def y2indicator(y):
     N = len(y)
@@ -101,13 +79,11 @@ def y2indicator(y):
         ind[i, y[i]] = 1
     return ind
 
-
 def benchmark_full():
     X, Y = get_normalized_data()
-
     print "Performing logistic regression..."
     # lr = LogisticRegression(solver='lbfgs')
-
+    
     # # test on the last 1000 points
     # lr.fit(X[:-1000, :200], Y[:-1000]) # use only first 200 dimensions
     # print lr.score(X[-1000:, :200], Y[-1000:])
@@ -171,22 +147,18 @@ def benchmark_full():
     plt.plot(CRtest)
     plt.show()
 
-
 def benchmark_pca():
     X, Y, _, _ = get_transformed_data()
     X = X[:, :300]
-
     # normalize X first
     mu = X.mean(axis=0)
     std = X.std(axis=0)
     X = (X - mu) / std
-
     print "Performing logistic regression..."
     Xtrain = X[:-1000,]
     Ytrain = Y[:-1000]
     Xtest  = X[-1000:,]
     Ytest  = Y[-1000:]
-
     N, D = Xtrain.shape
     Ytrain_ind = np.zeros((N, 10))
     for i in xrange(N):
@@ -224,7 +196,6 @@ def benchmark_pca():
         if i % 10 == 0:
             print "Cost at iteration %d: %.6f" % (i, ll)
             print "Error rate:", err
-
     p_y = forward(Xtest, W, b)
     print "Final error rate:", error_rate(p_y, Ytest)
     iters = range(len(LL))
@@ -232,7 +203,6 @@ def benchmark_pca():
     plt.show()
     plt.plot(CRtest)
     plt.show()
-
 
 if __name__ == '__main__':
     benchmark_pca()

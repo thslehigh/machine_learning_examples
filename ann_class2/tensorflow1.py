aa093ddd83c97a9e9b15e630bd1d3f49ea5e4bc8
thslehigh <thethansoe@gmail.com>
2016-08-17 19:17:49 -0600

    Update tensorflow1.py

--- ann_class2/tensorflow1.py
+++ ann_class2/tensorflow1.py
@@ -3,25 +3,17 @@
 # and shows how you can optimize a function.
 # I compare this to theano1.py multiple times.
 # So you might want to check that out first.
-
-# For the class Data Science: Practical Deep Learning Concepts in Theano and TensorFLow
-# https://www.udemy.com/data-science-deep-learning-in-theano-tensorflow
-
 import numpy as np
 import tensorflow as tf
-
 
 # you have to specify the type
 A = tf.placeholder(tf.float32, shape=(5, 5), name='A')
 
-
 # but shape and name are optional
 v = tf.placeholder(tf.float32)
 
-
 # I think this name is more appropriate than 'dot'
 w = tf.matmul(A, v)
-
 
 # similar to Theano, you need to "feed" the variables values.
 # In TensorFlow you do the "actual work" in a "session".
@@ -34,9 +26,7 @@ with tf.Session() as session:
 
     # what's this output that is returned by the session? let's print it
     print output, type(output)
-
     # luckily, the output type is just a numpy array. back to safety!
-
 
 # TensorFlow variables are like Theano shared variables.
 # But Theano variables are like TensorFlow placeholders.
@@ -59,7 +49,6 @@ with tf.Session() as session:
     # eval() in tf is like get_value() in Theano
     print x.eval() # the initial value of x
     print t.eval()
-
 
 # let's now try to find the minimum of a simple cost function like we did in Theano
 u = tf.Variable(20.0)
